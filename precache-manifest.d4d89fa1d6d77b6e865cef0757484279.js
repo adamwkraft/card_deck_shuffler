1bb6025efb97621481891f78c062668980052570
Adam Kraft <adamwkraft@gmail.com>
2020-05-23 16:51:08 -0700

    Updates

---
+++ precache-manifest.d4d89fa1d6d77b6e865cef0757484279.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5faba05194ccb8bc82d616c1b94b5982",
+    "url": "/card_deck_shuffler/index.html"
+  },
+  {
+    "revision": "15b19ed4aa7d1843bdb7",
+    "url": "/card_deck_shuffler/static/css/main.c62e34fb.chunk.css"
+  },
+  {
+    "revision": "b57dad7f416c16776855",
+    "url": "/card_deck_shuffler/static/js/2.1a78bfbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/card_deck_shuffler/static/js/2.1a78bfbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15b19ed4aa7d1843bdb7",
+    "url": "/card_deck_shuffler/static/js/main.7dffcf6a.chunk.js"
+  },
+  {
+    "revision": "b3acf0a994cbf9fd2772",
+    "url": "/card_deck_shuffler/static/js/runtime-main.d1a96882.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/card_deck_shuffler/static/media/logo.5d5d9eef.svg"
+  }
+]);
